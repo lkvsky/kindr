ba5d24b1e8b9c8cc35c22dd8701dcea7d105e4c8
Kyle Lucovsky <kyle@lucovsky.com>
2015-02-17 11:45:32 -0500

    Initial Commit

---
+++ Kindr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kindr
+//
+//  Created by Kyle Lucovsky on 2/17/15.
+//  Copyright (c) 2015 Kyle Lucovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kindr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Kindr
+//
+//  Created by Kyle Lucovsky on 2/17/15.
+//  Copyright (c) 2015 Kyle Lucovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
